5d6448b8b6d44e012326e01939c3466894b70961
Rigney <krigney@proforma.com>
2018-04-02 16:45:41 -0400

    More Styling added.

--- public/main.js
+++ public/main.js
@@ -3,16 +3,16 @@
 const max = 18418;
 
 const generateRandomCategoryNumber = () => {
- let randomNum = Math.floor(Math.random()* max)
- console.log(randomNum)
-//  thisGameCategories.push(randomNum);
-return randomNum
+  let randomNum = Math.floor(Math.random() * max)
+  console.log(randomNum)
+  //  thisGameCategories.push(randomNum);
+  return randomNum
 }
 
 const createCategory = (data) => {
   return {
-    title :data.title,
-    clues: data.clues.slice(0,5)
+    title: data.title,
+    clues: data.clues.slice(0, 5)
   }
 }
 
@@ -25,63 +25,65 @@ const createCategory = (data) => {
 
 angular
   .module("jeopardyApp", [])
-  .controller("mainController",["$scope", "$http", ($scope, $http) => {
+  .controller("mainController", ["$scope", "$http", ($scope, $http) => {
 
-  $scope.thisGameCategories = [];
+    $scope.thisGameCategories = [];
 
-  for (let i = 0; i < 5; i++){
-    $http({
-      method: "GET",
-      url: `http://jservice.io/api/category?id=${generateRandomCategoryNumber()}`
-    }).then(response => {
-      console.log(response);
-      const cat = createCategory(response.data);
-      $scope.thisGameCategories.push(cat);
-      console.log({cat, all: $scope.thisGameCategories})
-    })
-  }
+    for (let i = 0; i < 5; i++) {
+      $http({
+        method: "GET",
+        url: `http://jservice.io/api/category?id=${generateRandomCategoryNumber()}`
+      }).then(response => {
+        console.log(response);
+        const cat = createCategory(response.data);
+        $scope.thisGameCategories.push(cat);
+        console.log({ cat, all: $scope.thisGameCategories })
+      })
+    }
 
 
     //player selects a question
     //>>>>>>>>>board is hidden and selected question appears 
-    
-  $scope.selectedQuestion ="";
 
-  $scope.getQuestion = (clue) => {
-    console.log("button was pushed")
-    $scope.showQuestion=true;
-    $scope.selectedQuestion = clue;
-  }
-  
-  $scope.currentPlayerTotal = 0;
+    $scope.selectedQuestion = "";
 
-     //player can input a answer
-     //evaluate answer
-        //if answer is correct and uses a question, player gets question value added to their total
-        //if answer is correct and but they don't answer in a question, player gets question value subtracted from their total
-        //if answer is wrong, player gets question value subtracted from their total
+    $scope.getQuestion = (clue) => {
+      console.log("button was pushed")
+      $scope.showQuestion = true;
+      $scope.selectedQuestion = clue;
+      $scope.hideValue = true;
+    }
 
-  $scope.checkAnswer = () => {
-    console.log($scope.selectedQuestion)
-    //>>>>>>>>>need to address the special character issue Tirzah was talking about?
-    if ($scope.selectedQuestion.answer === $scope.playerAnswer) {
-      $scope.currentPlayerTotal += $scope.selectedQuestion.value
-  } else {
-      $scope.currentPlayerTotal -= $scope.selectedQuestion.value
-  }
-  }
+    $scope.currentPlayerTotal = 0;
+
+    //player can input a answer
+    //evaluate answer
+    //if answer is correct and uses a question, player gets question value added to their total
+    //if answer is correct and but they don't answer in a question, player gets question value subtracted from their total
+    //if answer is wrong, player gets question value subtracted from their total
+
+    $scope.checkAnswer = () => {
+      console.log($scope.selectedQuestion)
+      //>>>>>>>>>need to address the special character issue Tirzah was talking about?
+      if ($scope.selectedQuestion.answer === $scope.playerAnswer) {
+        $scope.currentPlayerTotal += $scope.selectedQuestion.value
+      } else {
+        $scope.currentPlayerTotal -= $scope.selectedQuestion.value
+      }
+    }
+
 
 
   }]);
 
 
-  //player input functionality: 
+//player input functionality: 
 
-  // board displays again
-    // player can no longer select questions containers they have already selected
+// board displays again
+// player can no longer select questions containers they have already selected
 
-  //game is played until no more containers are available
-    //play again? button appears to reset board and pull in 5 new categories
+//game is played until no more containers are available
+//play again? button appears to reset board and pull in 5 new categories
 
 
 document.addEventListener('DOMContentLoaded', generateRandomCategoryNumber)
