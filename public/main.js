8a6a32e80b21faf26f963af90aa9893e4cc44f24
Rigney <krigney@proforma.com>
2018-04-02 14:27:17 -0400

    Questions are displayed upon button click. Need to separate button click functionality-it's displaying every clue on one button click. Working on player answering and score totalling.

--- public/main.js
+++ public/main.js
@@ -5,29 +5,65 @@ const max = 18418;
 const generateRandomCategoryNumber = () => {
  let randomNum = Math.floor(Math.random()* max)
  console.log(randomNum)
- return randomNum
+//  thisGameCategories.push(randomNum);
+return randomNum
 }
+
 
 
 //use that random number to call a jeopardy category from the API
 //, 5 times
-const BASE_URL = `http://jservice.io/api/category?id=${generateRandomCategoryNumber()}`;
+
+// const BASE_URL = `http://jservice.io/api/category?id=${generateRandomCategoryNumber()}`;
+
 
 angular
   .module("jeopardyApp", [])
   .controller("mainController",["$scope", "$http", ($scope, $http) => {
-  // for (let i = 0; i < 5; i++){
+
+  $scope.thisGameCategories = [];
+
+  for (let i = 0; i < 5; i++){
     $http({
       method: "GET",
-      url: BASE_URL
+      url: `http://jservice.io/api/category?id=${generateRandomCategoryNumber()}`
     }).then(response => {
       $scope.categoryTitle = response.data.title;
       $scope.category = response.data.clues;
-      console.log(response.data);
-      console.log($scope.category)
-      console.log($scope.categoryTitle)
+      //display 5 categories in 5 columns of 5 rows in the DOM
+      $scope.thisGameCategories.push(response.data)
     })
-  // }
+
+    console.log($scope.thisGameCategories)
+  }
+
+
+    //player selects a question
+    //board is hidden and selected question appears 
+    
+  $scope.getQuestion = (clue) => {
+    console.log("button was pushed")
+    $scope.showClue=true;
+    $scope.selectedQuestion = $scope.category[];
+  }
+  
+  $scope.currentPlayerTotal = 0;
+
+     //player can input a answer
+     //evaluate answer
+        //if answer is correct and uses a question, player gets question value added to their total
+        //if answer is correct and but they don't answer in a question, player gets question value subtracted from their total
+        //if answer is wrong, player gets question value subtracted from their total
+
+
+  $scope.checkAnswer = () => {
+    if ($scope.selectedQuestion.answer.replace(/(\W|\S)/g, '').toLowerCase() === $scope.playerAnswer.replace(/(\W|\S)/g, '').toLowerCase()) {
+      $scope.currentPlayerTotal += $scope.selectedQuestion.value
+  } else {
+      $scope.currentPlayerTotal -= $scope.selectedQuestion.value
+  }
+  }
+
 
   }]);
 
@@ -37,19 +73,7 @@ angular
 
 
 
-//display 5 categories in 5 columns of 5 rows in the DOM
-  /// row one will be the name of the category
-  //rows 2-5 will be the questionsvalue
-
-  //player selects a question
-    //board is hidden and selected question appears 
-
   //player input functionality: 
-   //player can input a answer
-     //evaluate answer
-        //if answer is correct and uses a question, player gets question value added to their total
-        //if answer is correct and but they don't answer in a question, player gets question value subtracted from their total
-        //if answer is wrong, player gets question value subtracted from their total
 
   // board displays again
     // player can no longer select questions containers they have already selected
